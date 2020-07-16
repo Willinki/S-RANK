66045a5a6c277545a05e6f0659118d7f43cd9e35
Willinki <davide.badalotti@outlook.it>
2020-07-16 18:02:25 +0200

    reorganizing repo as module

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+        name='S-RANK',
+        version='0.5',
+        description='A basic implementation of the Scalable RANK Algorithm, for feature selection in unsupervised learning problems.',
+        url='https://github.com/Willinki/S-RANK.git',
+        author='Davide Badalotti',
+        author_email='davide.badalotti@outlook.it',
+        license='GPL-3.0',
+        packages=['S-RANK'], 
+        zip_safe=False
+     )
+
+        
+
